9a9010ec91a100f4ce338329de0a39a0db3361b0
t-sin <shinichi.tanaka45@gmail.com>
2021-10-19 10:34:36 +0900

    Update OOP example

--- examples/06-oop.lisp
+++ examples/06-oop.lisp
@@ -3,21 +3,21 @@
 ;; creating classes
 ;; `#{}` is a hash table literal
 ;; class is a hash table that has a hash table named as `*meta*`
-(setq *parent-object*
-  #{:*meta* #{:name :parent :parent nil}
-    :method (lambda (self) (println "Ku!"))})
+(setq *animal*
+  #{:*meta* #{:name :animal :parent nil}
+    :say (lambda (self) (println "Animal!"))})
 ; => #{...}
 
-(setq *child-object*
-  #{:*meta* #{:name :child :parent *parent-object*}
-    :method (lambda (self)
-              (send :method (get (get self :*meta*) :parent))
-              (println "Kyu!!"))})
+(setq *dodo*
+  #{:*meta* #{:name :dodo :parent *animal*}
+    :say (lambda (self)
+              (send :say (get (get self :*meta*) :parent))
+              (println "Dodo!!"))})
 ; => #{...}
 
-(send :method *parent-object*)
-; => Ku!
+(send :say *animal*)
+; => Animal!
 
-(send :method *child-object*)
-; => Ku!
-;    Kyu!!
+(send :say *dodo*)
+; => Animal!
+;    Dodo!!
