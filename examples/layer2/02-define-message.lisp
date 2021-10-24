bb6fb473031b1005b3aeb49f4efe2b6c16928591
t-sin <shinichi.tanaka45@gmail.com>
2021-10-25 05:42:37 +0900

    Add some examples for layer2

---
+++ examples/layer2/02-define-message.lisp
@@ -0,0 +1,15 @@
+;; message definition example
+
+(defmsg pulse ((ph float) (duty float))
+  (if (> ph duty)
+      1.0
+      0.0))
+
+(pulse 0.0 0.5)
+; => 0.0
+
+(pulse 0.8 0.5)
+; => 1.0
+
+(pulse 1 1)
+; -> unknown message (because `1` is integer)
