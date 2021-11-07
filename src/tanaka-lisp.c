80931b37f83bef1f4e6f7c5c73e4a7a8b35338f9
t-sin <shinichi.tanaka45@gmail.com>
2021-11-08 05:40:08 +0900

    Echo REPL

--- src/tanaka-lisp.c
+++ src/tanaka-lisp.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+char *tlisp_version = "0.0.0";
+
+#define LINE_SIZE 512
+
 int main(int argc, char **argv) {
-    printf("tanaka-lisp.\n");
+    printf("tanaka-lisp v%s\n", tlisp_version);
+
+    char linebuf[LINE_SIZE];
+
+    while (1) {
+        printf("tanaka-lisp> ");
+
+        if (fgets(linebuf, LINE_SIZE, stdin) > 0) {
+            printf("%s", linebuf);
+
+        } else {
+            printf("\nbye.\n");
+            break;
+        }
+    }
+
     return 0;
 }
