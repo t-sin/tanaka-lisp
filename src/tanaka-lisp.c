6d3bc3a85df96db87bb8ec839a5a6fbc3913dbbd
t-sin <shinichi.tanaka45@gmail.com>
2021-11-24 23:04:48 +0900

    WIP: Echo REPL

--- src/tanaka-lisp.c
+++ src/tanaka-lisp.c
@@ -1,8 +1,24 @@
+#include <stdint.h>
 #include <stdio.h>
 
 char *tlisp_version = "0.0.0";
 
 #define LINE_SIZE 512
+
+typedef enum {
+    BOOLE,
+} tLispType;
+
+typedef struct tLispObject_t {
+    tLispType type;
+    union {
+        uint8_t bool;
+    } o;
+} tLispObject;
+
+int tLisp_read(FILE *in, tLispObject *out_obj) {
+    return 0;
+}
 
 int main(int argc, char **argv) {
     printf("tanaka-lisp v%s\n", tlisp_version);
