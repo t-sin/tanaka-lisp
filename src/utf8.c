834a98c0de8a3c78a3cc4e61d4d03185344a0c01
t-sin <shinichi.tanaka45@gmail.com>
2021-11-27 20:37:45 +0900

    Decode all length UTF-8 octests

--- src/utf8.c
+++ src/utf8.c
@@ -4,13 +4,13 @@
 #include "utf8.h"
 
 int t_utf8_length(tByte first_byte) {
-    if (!(first_byte & 0x80)) {
+    if ((first_byte & 0x80) == 0x00) {
         return 1;
-    } else if (first_byte & 0xc0) {
+    } else if ((first_byte & 0xe0) == 0xc0) {
         return 2;
-    } else if (first_byte & 0xe0) {
+    } else if ((first_byte & 0xf0) == 0xe0) {
         return 3;
-    } else if (first_byte & 0xf0) {
+    } else if ((first_byte & 0xf8) == 0xf0) {
         return 4;
     } else {
         return UTF8_INVALID_OCTETS;
@@ -28,25 +28,25 @@ int t_utf8_decode(const tByte *bytes4, tChar *out_char) {
         return 1;
 
     case 2:
-        tmp = (bytes4[0] & 0x01ff) << 6;
-        tmp = (bytes4[1] & 0x03ff) | tmp;
+        tmp = (bytes4[0] & 0x1f) << 6;
+        tmp = (bytes4[1] & 0x3f) | tmp;
         *out_char = tmp;
 
         return 2;
 
     case 3:
-        tmp = (bytes4[0] & 0x00ff) << 12;
-        tmp = (bytes4[1] & 0x03ff) << 6 | tmp;
-        tmp = (bytes4[2] & 0x03ff) | tmp;
+        tmp = (bytes4[0] & 0x0f) << 12;
+        tmp = (bytes4[1] & 0x3f) << 6 | tmp;
+        tmp = (bytes4[2] & 0x3f) | tmp;
         *out_char = tmp;
 
         return 3;
 
     case 4:
-        tmp = (bytes4[0] & 0x0007) << 18;
-        tmp = (bytes4[1] & 0x03ff) << 12 | tmp;
-        tmp = (bytes4[2] & 0x03ff) << 6 | tmp;
-        tmp = (bytes4[3] & 0x03ff) | tmp;
+        tmp = (bytes4[0] & 0x07) << 18;
+        tmp = (bytes4[1] & 0x3f) << 12 | tmp;
+        tmp = (bytes4[2] & 0x3f) << 6 | tmp;
+        tmp = (bytes4[3] & 0x3f) | tmp;
         *out_char = tmp;
 
         return 4;
@@ -80,8 +80,35 @@ static void test_utf8_decode_ascii_one() {
     verify_utf8_decode(input, expected_ret, expected_ch);
 }
 
+static void test_utf8_decode_greek_one() {
+    tByte input[INPUT_SIZE] = {0xce, 0xbb};
+    int expected_ret = 2;
+    tChar expected_ch = 0x03bb; // 'λ'
+
+    verify_utf8_decode(input, expected_ret, expected_ch);
+}
+
+static void test_utf8_decode_hiragana_one() {
+    tByte input[INPUT_SIZE] = {0xe3, 0x81, 0x82};
+    int expected_ret = 3;
+    tChar expected_ch = 0x3042; // 'あ'
+
+    verify_utf8_decode(input, expected_ret, expected_ch);
+}
+
+static void test_utf8_decode_partying_face_one() {
+    tByte input[INPUT_SIZE] = {0xf0, 0x9f, 0xa5, 0xb3};
+    int expected_ret = 4;
+    tChar expected_ch = 0x1f973; // '🥳'
+
+    verify_utf8_decode(input, expected_ret, expected_ch);
+}
+
 void test_utf8_all() {
     test_utf8_decode_ascii_one();
+    test_utf8_decode_greek_one();
+    test_utf8_decode_hiragana_one();
+    test_utf8_decode_partying_face_one();
 
     printf("test: utf8 -> ok\n");
 }
