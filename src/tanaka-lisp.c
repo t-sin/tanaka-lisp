62faaa685fde69a8fa40c2b320011e6f13479c30
t-sin <shinichi.tanaka45@gmail.com>
2021-11-27 15:23:59 +0900

    REPL recieves boolean values `#f` and `#t`

--- src/tanaka-lisp.c
+++ src/tanaka-lisp.c
@@ -1,35 +1,117 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "binary_stream.h"
+#include "stream.h"
+#include "tanaka_type.h"
 
 char *tlisp_version = "0.0.0";
 
-#define LINE_SIZE 512
+#define READ_NO_INPUT 0
+#define READ_FAILED -1
 
-typedef enum {
-    BOOLE,
-} tLispType;
+int tLisp_read(tStream *in, tLispObject *out_obj) {
+    size_t num = 0;
+    tChar ch;
+    int ret;
 
-typedef struct tLispObject_t {
-    tLispType type;
-    union {
-        uint8_t bool;
-    } o;
-} tLispObject;
+    while (ret = t_stream_peek_char(in, &ch), ret > 0) {
+        switch (ch) {
+        case '#':
+            t_stream_read_char(in, &ch);
+            num++;
 
-int tLisp_read(FILE *in, tLispObject *out_obj) {
+            if (t_stream_peek_char(in, &ch) <= 0) {
+                return READ_FAILED;
+            }
+            t_stream_read_char(in, &ch);
+            num++;
+
+            switch (ch) {
+            case 'f':
+                out_obj->type = BOOLE;
+                out_obj->o.bool = 0;
+                break;
+            case 't':
+                out_obj->type = BOOLE;
+                out_obj->o.bool = 1;
+            default:
+                return num;
+            }
+            break;
+
+        default:
+            return num;
+        }
+    }
+
+    return num;
+}
+
+int tLisp_eval(tLispObject *obj, tLispObject *out_obj) {
+    *out_obj = *obj;
+
     return 0;
 }
+
+void tLisp_print(tStream *out, tLispObject *obj) {
+    switch (obj->type) {
+    case BOOLE:
+        t_stream_write_byte(out, '#');
+        t_stream_write_byte(out, obj->o.bool == 0 ? 'f' : 't');
+        break;
+
+    default:
+        break;
+    }
+}
+
+#define LINE_SIZE 512
 
 int main(int argc, char **argv) {
     printf("tanaka-lisp v%s\n", tlisp_version);
 
     char linebuf[LINE_SIZE];
+    memset(linebuf, 0, LINE_SIZE);
+
+    tStream *stream_stdin = make_stream(make_binary_stream());
+    tStream *stream_stdout = make_stream(make_binary_stream());
+    int more_input_needed = 0;
 
     while (1) {
-        printf("tanaka-lisp> ");
+        if (more_input_needed) {
+            printf("...        > ");
+            more_input_needed = 0;
+        } else {
+            printf("tanaka-lisp> ");
+        }
 
         if (fgets(linebuf, LINE_SIZE, stdin) > 0) {
-            printf("%s", linebuf);
+            for (int i = 0; linebuf[i] != '\n'; i++) {
+                t_stream_write_byte(stream_stdin, linebuf[i]);
+            }
+
+            tLispObject obj;
+            int ret = tLisp_read(stream_stdin, &obj);
+
+            if (ret == 0) {
+                continue;
+            } else if (ret == -1) {
+                more_input_needed = 1;
+                continue;
+            }
+
+            tLispObject eobj;
+            tLisp_eval(&obj, &eobj);
+
+            tLisp_print(stream_stdout, &eobj);
+
+            tByte b;
+            while (t_stream_read_byte(stream_stdout, &b) > 0) {
+                fputc(b, stdout);
+            }
+            puts("");
 
         } else {
             printf("\nbye.\n");
