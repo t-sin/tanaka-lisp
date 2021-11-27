1527a85352fce80390a10475b1d39af1a04b34a6
t-sin <shinichi.tanaka45@gmail.com>
2021-11-27 22:15:38 +0900

    More documentation

--- src/utf8.h
+++ src/utf8.h
@@ -11,13 +11,14 @@
 int t_utf8_length(tByte first_byte);
 
 // Decodes UTF-8 octets.
-// If this reads valid bytes it returns a number of bytes read, otherwise `UTF8_INVALID_OCTETS`.
+// If this reads valid bytes it returns a number of bytes decoded, otherwise `UTF8_INVALID_OCTETS`.
 // It assumes that `bytes4` has 4-byte length.
 //
 // Specification fo UTF-8 octets: https://datatracker.ietf.org/doc/html/rfc3629#section-4
 int t_utf8_decode(const tByte *bytes4, tChar *out_char);
 
 // Encodes Unicode codepoint and write bytes into `out_4bytes`.
+// It returns a number of bytes encoded.
 // It assumes that `out_bytes4` has 4-byte length.
 int t_utf8_encode(tChar ch, tByte *out_bytes4);
 
