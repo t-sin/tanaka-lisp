b7a0fa7367e8d7b1a5d6ce1c4740f298773f4994
t-sin <shinichi.tanaka45@gmail.com>
2021-11-27 17:03:45 +0900

    Add integer type

--- src/tanaka_type.h
+++ src/tanaka_type.h
@@ -4,16 +4,18 @@
 #include <stdint.h>
 
 typedef uint8_t tByte;
+typedef int64_t tInt;
 typedef uint32_t tChar;
 
 typedef enum {
-    BOOLE,
+    BOOLE, INTEGER,
 } tLispType;
 
 typedef struct tLispObject_t {
     tLispType type;
     union {
         uint8_t bool;
+        tInt intn;
     } o;
 } tLispObject;
 
