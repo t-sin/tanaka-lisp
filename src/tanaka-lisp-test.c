669645050270a541b47b23347acba8bc4d6e2d7b
t-sin <shinichi.tanaka45@gmail.com>
2021-11-27 11:35:09 +0900

    Run binary streams' tests

--- src/tanaka-lisp-test.c
+++ src/tanaka-lisp-test.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 #include "utf8.h"
+#include "binary_stream.h"
 
 int main(int argc, char **argv) {
     printf("running tanaka-lisp tests...\n");
 
     test_utf8_all();
+    test_binary_stream_all();
 
     printf("all tests ok.\n");
     return 0;
