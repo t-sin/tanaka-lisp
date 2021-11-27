fa7c36323ce7eb421fc4036047561f1397f818d2
t-sin <shinichi.tanaka45@gmail.com>
2021-11-27 11:46:35 +0900

    Read a byte from bnary streams

--- src/binary_stream.c
+++ src/binary_stream.c
@@ -49,7 +49,21 @@ int t_stream_peek_byte(tBinaryStream *stream, tByte *out_byte) {
     return 0;
 }
 
-int t_stream_read_byte(tBinaryStream *stream, tByte *out_byte) {}
+static void proceed_head(tBinaryStream *stream) {
+    stream->head = (stream->head + 1) % STREAM_BUFFER_SIZE;
+}
+
+int t_stream_read_byte(tBinaryStream *stream, tByte *out_byte) {
+    if (stream == NULL || stream_buffer_length(stream) <= 0) {
+        return STREAM_EMPTY;
+    }
+
+    size_t read_pos = calculate_read_pos(stream);
+    *out_byte = stream->array[read_pos];
+    proceed_head(stream);
+
+    return 1;
+}
 
 int t_stream_write_byte(tBinaryStream *stream, tByte byte) {}
 
@@ -88,9 +102,41 @@ static void test_peek_byte_one() {
     assert(input.head == expected_head);
 }
 
+static void test_read_byte_from_empty_stream() {
+    tBinaryStream input = {NULL, 0, 0};
+    int expected_ret = STREAM_EMPTY;
+
+    tByte actual_byte;
+    int actual_ret = t_stream_read_byte(&input, &actual_byte);
+
+    assert(actual_ret == expected_ret);
+}
+
+static void verify_read_byte(tBinaryStream *input, int expected_ret, tByte expected_byte) {
+    tByte actual_byte;
+    int actual_ret = t_stream_read_byte(input, &actual_byte);
+
+    assert(actual_ret == expected_ret);
+    assert(actual_byte == expected_byte);
+}
+
+static void test_read_byte_one() {
+    tByte input_buf[STREAM_BUFFER_SIZE] = {'a', 0, 0};
+    tBinaryStream input = {input_buf, 1, 0};
+    int expected_ret = 1;
+    tByte expected_byte = 'a';
+    size_t expected_head = 2;
+
+    verify_read_byte(&input, expected_ret, expected_byte);
+    assert(input.head == expected_head);
+}
+
 void test_binary_stream_all() {
     test_peek_byte_from_empty_stream();
     test_peek_byte_one();
+
+    test_read_byte_from_empty_stream();
+    test_read_byte_one();
 
     printf("test: binary stream -> ok\n");
 }
